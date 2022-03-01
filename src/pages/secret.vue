a5a447d7f035498df4800bdd7aa42e33d11f1830
Eric Canales <ecanal16@stu.southtexascollege.edu>
2022-03-01 15:29:33 -0600

    secret page edited

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,7 @@
 <template>
+    <div>
+        <img src="../assets/bgsecret.jpeg" alt="secret image">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt placeat adipisci dolore, aspernatur id in animi alias praesentium. Totam saepe quos amet quasi praesentium doloribus, autem corporis minus molestiae obcaecati.</p>
+    <img src="../assets/bgsecret.jpeg" alt="secret image">
+    </div>
 </template>
