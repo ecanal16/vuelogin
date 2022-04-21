6edde1c589729bb6b824a5227845dea5da5e037f
Eric Canales <ecanal16@stu.southtexascollege.edu>
2022-04-21 13:30:15 -0500

    chat layout

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <img src="../assets/bgsecret.jpeg" alt="secret image">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt placeat adipisci dolore, aspernatur id in animi alias praesentium. Totam saepe quos amet quasi praesentium doloribus, autem corporis minus molestiae obcaecati.</p>
-    <img src="../assets/bgsecret.jpeg" alt="secret image">
+    <h1 class="text-6xl font-extrabold tracking-tighter text-center mt-8">My Chat</h1>
+    <div class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between">
+        <ul class="p-4 space-y-4">
+            <li v-for="n in 5">
+                <div class="flex justify-between bg-gray-200 px-4 py-2 rounded-lg"><span>Message itself</span><span>by Author Name</span></div>
+            </li>
+        </ul>
+        <div>
+            <input class="w-full p-4 rounded-lg focus:outline-none focus:bg-blue-100" type="text" placeholder="Type a message..."/>
+        </div>
     </div>
 </template>
